51d03f700b5c7f2a706b963f9dfa79a537e6edab
Carlos Barranco <barrancocarlos@hotmail.com>
2023-10-03 23:24:16 -0300

    model

--- sensors/models.py
+++ sensors/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Temperature(models.Model):
+  value = models.CharField(max_length=200, default='00')
+  creation_date = models.DateTimeField(auto_now_add=True)
+  
+  def __str__(self):
+        return self.value

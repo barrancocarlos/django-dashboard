79dbf21457443c928a54e472a68695cfb243d777
Carlos Barranco <barrancocarlos@hotmail.com>
2023-11-06 20:07:43 -0300

    models fix

--- sensors/models.py
+++ sensors/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Temperature(models.Model):
-  value = models.IntegerField(max_length=200, default='00')
+  value = models.FloatField(max_length=200, default='00')
   creation_date = models.DateTimeField(auto_now_add=True)
   
   
   
   def __str__(self):
-        return self.value
+        return str(self.value)
